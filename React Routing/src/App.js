69801b28de7a2f7b819490ee4cad6a047c447e47
MTahaqr <tahaqr28@outlook.com>
2017-03-28 22:30:41 +0500

    Material UI Added

--- React Routing/src/App.js
+++ React Routing/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Link to="/About">About</Link>
+        {/*<Link to="/About">About</Link>
         <br/>
          <Link to="/Contact">Contact</Link>
          <br/>
@@ -15,8 +15,55 @@ class App extends Component {
            <Link to="/Inf">Inf</Link>
             <br/>
             <Link to="/Profile">Profile</Link>
-            <br/>
-            {this.props.children}
+            <br/>*/}
+           
+
+
+            {/*<!-- Always shows a header, even in smaller screens. -->*/}
+<div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header className="mdl-layout__header">
+    <div className="mdl-layout__header-row">
+      {/*<!-- Title -->*/}
+      <span className="mdl-layout-title">Title</span>
+      {/*<!-- Add spacer, to align navigation to the right -->*/}
+      <div className="mdl-layout-spacer"></div>
+      {/*<!-- Navigation. We hide it in small screens. -->*/}
+      <nav className="mdl-navigation mdl-layout--large-screen-only">
+        <Link to="/About" className="mdl-navigation__link" >About</Link>
+        
+         <Link to="/Contact" className="mdl-navigation__link">Contact</Link>
+         
+          <Link to="/Home" className="mdl-navigation__link">Home</Link>
+           
+           <Link to="/Inf" className="mdl-navigation__link">Inf</Link>
+            
+            <Link to="/Profile" className="mdl-navigation__link">Profile</Link>
+            
+      </nav>
+    </div>
+  </header>
+  <div className="mdl-layout__drawer">
+    <span className="mdl-layout-title">Title</span>
+    <nav className="mdl-navigation">
+      <Link to="/About" className="mdl-navigation__link">About</Link>
+        
+         <Link to="/Contact" className="mdl-navigation__link">Contact</Link>
+         
+          <Link to="/Home" className="mdl-navigation__link">Home</Link>
+           
+           <Link to="/Inf" className="mdl-navigation__link">Inf</Link>
+            
+            <Link to="/Profile" className="mdl-navigation__link">Profile</Link>
+            
+    </nav>
+  </div>
+  <main className="mdl-layout__content">
+    <div className="page-content">
+       {this.props.children}
+      {/*<!-- Your content goes here -->*/}
+      </div>
+  </main>
+</div>
  
       </div>
     );
--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -5,15 +5,38 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  //  <!-- Always shows a header, even in smaller screens. -->
+<div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header className="mdl-layout__header">
+    <div className="mdl-layout__header-row">
+      {/*<!-- Title -->*/}
+      <span className="mdl-layout-title">Title</span>
+      {/*<!-- Add spacer, to align navigation to the right -->*/}
+      <div className="mdl-layout-spacer"></div>
+      {/*<!-- Navigation. We hide it in small screens. -->*/}
+      <nav className="mdl-navigation mdl-layout--large-screen-only">
+        <a className="mdl-navigation__link" href="">Link</a>
+        <a className="mdl-navigation__link" href="">Link</a>
+        <a className="mdl-navigation__link" href="">Link</a>
+        <a className="mdl-navigation__link" href="">Link</a>
+      </nav>
+    </div>
+  </header>
+  <div className="mdl-layout__drawer">
+    <span className="mdl-layout-title">Title</span>
+    <nav className="mdl-navigation">
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+    </nav>
+  </div>
+  <main className="mdl-layout__content">
+    <div className="page-content">
+      {/*<!-- Your content goes here -->*/}
       </div>
+  </main>
+</div>
     );
   }
 }
