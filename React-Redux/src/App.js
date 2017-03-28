88f17b117b01be8afad8118a683e77c722f51ac3
MTahaqr <tahaqr28@outlook.com>
2017-03-29 04:30:50 +0500

    remove material UI from react-redux

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -5,38 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-  //  <!-- Always shows a header, even in smaller screens. -->
-<div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header className="mdl-layout__header">
-    <div className="mdl-layout__header-row">
-      {/*<!-- Title -->*/}
-      <span className="mdl-layout-title">Title</span>
-      {/*<!-- Add spacer, to align navigation to the right -->*/}
-      <div className="mdl-layout-spacer"></div>
-      {/*<!-- Navigation. We hide it in small screens. -->*/}
-      <nav className="mdl-navigation mdl-layout--large-screen-only">
-        <a className="mdl-navigation__link" href="">Link</a>
-        <a className="mdl-navigation__link" href="">Link</a>
-        <a className="mdl-navigation__link" href="">Link</a>
-        <a className="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-  </header>
-  <div className="mdl-layout__drawer">
-    <span className="mdl-layout-title">Title</span>
-    <nav className="mdl-navigation">
-      <a className="mdl-navigation__link" href="">Link</a>
-      <a className="mdl-navigation__link" href="">Link</a>
-      <a className="mdl-navigation__link" href="">Link</a>
-      <a className="mdl-navigation__link" href="">Link</a>
-    </nav>
-  </div>
-  <main className="mdl-layout__content">
-    <div className="page-content">
-      {/*<!-- Your content goes here -->*/}
-      </div>
-  </main>
-</div>
+<h1>aaaa</h1>
     );
   }
 }
